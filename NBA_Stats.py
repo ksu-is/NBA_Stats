552985d12fcb40ad7b6b66c99f5548afa0579762
jovon196 <jjame196@students.kennesaw.edu>
2025-04-09 15:54:49 -0400

    Update NBA_Stats.py
    Developed code

--- NBA_Stats.py
+++ NBA_Stats.py
@@ -5,13 +5,16 @@ from nba_api.stats.endpoints import leaguedashplayerstats
 import pandas as pd
 import re
 
-matched_players = []
+all_players = players._get_players()
+
+def get_stats():
+    print("Welcome to the NBA Stats Presenter!")
+    player_or_season = input("Enter the name of a player or a season (e.g.,'2022-23'): ").strip().lower()
+    if player_or_season == 'exit':
+        print("Exiting the program. Goodbye!")
+        return
 
 
-
-
-
-def get_stats
 
 
 
