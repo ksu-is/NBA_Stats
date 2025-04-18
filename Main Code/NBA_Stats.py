1fcec5594d233728aac05ecf555b3aad95cda1c6
jovon196 <jjame196@students.kennesaw.edu>
2025-04-18 17:28:20 -0400

    Update NBA_Stats.py

--- Main Code/NBA_Stats.py
+++ Main Code/NBA_Stats.py
@@ -3,6 +3,16 @@ import re
 import time
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.static import teams
+
+nba_teams = teams.get_teams()
+
+
+
+
+
+
+
 
 
 # Initialize the statistic finder function
@@ -65,18 +75,20 @@ def player_stats():
         career_df['Avg_STL'] = career_df['STL'] / career_df['GP']
         career_df['Avg_BLK'] = career_df['BLK'] / career_df['GP']
         career_df['Avg_TOV'] = career_df['TOV'] / career_df['GP']
-        career_df[['Avg_MIN','Avg_PTS', 'Avg_AST', 'Avg_REB', 'Avg_STL', 'Avg_BLK', 'Avg_TOV','FG_PCT','FT_PCT','FG3_PCT']] = career_df[['Avg_MIN','Avg_PTS', 'Avg_AST', 'Avg_REB', 'Avg_STL', 'Avg_BLK', 'Avg_TOV','FG_PCT','FT_PCT','FG3_PCT']].round(2)
-        season_stats = (career_df[['SEASON_ID','TEAM_ABBREVIATION','GP','MIN','Avg_PTS','Avg_AST','Avg_REB','Avg_STL','Avg_BLK','Avg_TOV','FG_PCT','FT_PCT','FG3_PCT']].copy())
-        season_stats = season_stats.rename(columns={'SEASON_ID': 'Season', 'TEAM_ABBREVIATION': 'Team', 'GP': 'GP', 'MIN': 'MIN', 'Avg_PTS': 'PPG', 'Avg_AST': 'APG', 'Avg_REB': 'RPG', 'Avg_STL': 'SPG', 'Avg_BLK': 'BPG', 'Avg_TOV': 'TOV', 'FG_PCT': 'FG%', 'FT_PCT': 'FT%', 'FG3_PCT': '3P%'})   
+        career_df['FG_PCT'] = (career_df['FG_PCT']*100).round(1)
+        career_df['FT_PCT'] = (career_df['FT_PCT']*100).round(1) 
+        career_df['FG3_PCT'] = (career_df['FG3_PCT']*100).round(1) 
+        career_df[['Avg_MIN','Avg_PTS', 'Avg_AST', 'Avg_REB', 'Avg_STL', 'Avg_BLK', 'Avg_TOV','FG_PCT','FT_PCT','FG3_PCT']] = career_df[['Avg_MIN','Avg_PTS', 'Avg_AST', 'Avg_REB', 'Avg_STL', 'Avg_BLK', 'Avg_TOV','FG_PCT','FT_PCT','FG3_PCT']].round(1)
+        season_stats = (career_df[['SEASON_ID','TEAM_ABBREVIATION' ,'GP','Avg_MIN','Avg_PTS','Avg_AST','Avg_REB','Avg_STL','Avg_BLK','Avg_TOV','FG_PCT','FT_PCT','FG3_PCT']].copy())
+        season_stats = season_stats.rename(columns={'SEASON_ID': 'Season', 'TEAM_ABBREVIATION': 'Team', 'GP': 'GP', 'Avg_MIN': 'MIN', 'Avg_PTS': 'PPG', 'Avg_AST': 'APG', 'Avg_REB': 'RPG', 'Avg_STL': 'SPG', 'Avg_BLK': 'BPG', 'Avg_TOV': 'TOV', 'FG_PCT': 'FG%', 'FT_PCT': 'FT%', 'FG3_PCT': '3P%'})   
 
         print("Career Stats for", full_name)
         print("-----------------------------------------------------")
         print(season_stats.to_string(index=False))
-
-        print("Legend:\nPPG = Points Per Game \nAPG = Assists Per Game \nRPG = Rebounds Per Game \nSPG = Steals Per Game \nBPG = Blocks Per Game \nTOV = Turnovers \nFG% = Field Goal Percentage \nFT% = Free Throw Percentage \n3P% = Three Point Percentage")
-
-
-
+        print("-----------------------------------------------------")
+        print("Legend:\nGP - Games Played \nMPG - Minutes Per Game \nPPG - Points Per Game \nAPG - Assists Per Game \nRPG - Rebounds Per Game \nSPG - Steals Per Game \nBPG - Blocks Per Game \nTOV - Turnovers \nFG% - Field Goal Percentage \nFT% - Free Throw Percentage \n3P% - Three Point Percentage")
+        
+        
 if __name__ == "__main__":
     player_stats()
 
