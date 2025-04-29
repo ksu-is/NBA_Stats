15f040a4c7be2deeb274601c14e3fef54a90061e
jovon196 <jjame196@students.kennesaw.edu>
2025-04-28 23:15:09 -0400

    Update NBA_Stats.py
    Edited table to display joint teams rather than "TOT" and also filtered out unused libraries and obsolete code.

--- NBA_Stats.py
+++ NBA_Stats.py
@@ -1,15 +1,9 @@
-import pandas as pd
-import re
 import time
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import playercareerstats
-from nba_api.stats.endpoints import leaguedashplayerstats
-from nba_api.stats.static import teams
 from tabulate import tabulate
 
-
 print("Welcome to the NBA Statistics Presenter! \nThis program allows you to search for NBA player stats.")
-
 
 # Initialize the statistic finder function
 def player_stats():
@@ -25,7 +19,6 @@ def player_stats():
        
         print("Searching for player...")
         time.sleep(1.5)  # Simulate a delay for the search
-
 
         # Use regex to find players with similar names
         matches = players.find_players_by_full_name(search)
@@ -52,50 +45,16 @@ def player_stats():
         player_id = selected_player['id']
         full_name = selected_player['full_name']
 
-
         print("Player found: {}".format(full_name))
-
 
         # Get player stats
         career_stats = playercareerstats.PlayerCareerStats(player_id=player_id)
         career_df = career_stats.get_data_frames()[0]
 
-
         if career_df.empty:
             print("No career stats available for this player.")
             continue
        
-        # Get team names
-        nba_teams = teams.get_teams()
-        team_name = {team['abbreviation']: team['full_name'] for team in nba_teams}
-
-
-        # Add historical team names(API only returns current teams)
-        team_name.update({
-             'BUF': 'Buffalo Braves',
-            'CHA': 'Charlotte Bobcats',
-            'CHH': 'Charlotte Hornets (original)',
-            'CIN': 'Cincinnati Royals',
-            'INA': 'Indianapolis Olympians',
-            'KCO': 'Kansas City-Omaha Kings',
-            'MIH': 'Milwaukee Hawks',
-            'MNL': 'Minneapoils Lakers',
-            'NJN': 'New Jersey Nets',
-            'NOH': 'New Orleans Hornets',
-            'NOK': 'New Orleans/Oklahoma City Hornets',
-            'NYN': 'New York Nets',
-            'PHW': 'Philadelphia Warriors',
-            'ROC': 'Rochester Royals',
-            'SDC': 'San Diego Clippers',
-            'SEA': 'Seattle SuperSonics',
-            'SFW': 'San Francisco Warriors',
-            'STL': 'St.Louis Hawks',
-            'SYR': 'Syracuse Nationals',
-            'VAN': 'Vancouver Grizzlies',
-            'WSB': 'Washington Bullets'
-        })
-
-
         # Calculations for averages and percentages
         career_df['MPG'] = career_df['MIN'] / career_df['GP']
         career_df['PPG'] = career_df['PTS'] / career_df['GP']
@@ -108,22 +67,28 @@ def player_stats():
         career_df['FT%'] = (career_df['FT_PCT']*100)
         career_df['3P%'] = (career_df['FG3_PCT']*100)
        
-        # Get True Shooting Percentage (TS%)
+        # Calculate True Shooting Percentage (TS%)
         career_df['TS%'] = career_df['PTS'] / (2 * (career_df['FGA'] + 0.44 * career_df['FTA']))
         career_df['TS%'] = (career_df['TS%'] * 100)
        
         # Round the stats to one decimal place
         career_df[['MPG','PPG', 'APG', 'RPG', 'SPG', 'BPG', 'TOV','FG%','FT%','3P%','TS%']] = career_df[['MPG','PPG', 'APG', 'RPG', 'SPG', 'BPG', 'TOV','FG%','FT%','3P%','TS%']].round(1)
 
-        # Map team abbreviations to full names
+        # Map team columm to team names
         career_df['Team'] = career_df['TEAM_ABBREVIATION']
+
+        # Loop through each season and replace 'TOT' with the actual team names
+        for season in career_df['SEASON_ID'].unique():
+            season_rows = career_df[career_df['SEASON_ID'] == season]
+            if 'TOT' in season_rows['TEAM_ABBREVIATION'].values:
+                teams_played = season_rows[season_rows['TEAM_ABBREVIATION'] != 'TOT']['TEAM_ABBREVIATION'].unique()
+                career_df.loc[(career_df['SEASON_ID'] == season) & (career_df['TEAM_ABBREVIATION'] == 'TOT'), 'Team'] = '/'.join(teams_played)
 
         # Filter the DataFrame to include only the relevant columns
         season_stats = career_df[['SEASON_ID','Team' ,'GP','MPG','PPG', 'APG', 'RPG', 'SPG', 'BPG', 'TOV','FG%','FT%','3P%','TS%']].copy()
        
         # Rename SEASON_ID column for clarity
         season_stats.rename(columns={'SEASON_ID': 'Season'}, inplace=True)
-
 
         # Format the season column to be more readable
         print("Career Stats for", full_name)
